653df83ed7fd0322dd87de387bcdab99cb6201da
Julio Sanchez G <je02sg@gmail.com>
2025-04-12 18:05:53 +0200

    Initial project setup

---
+++ StreakPlateMethod/StreakPlateMethod/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StreakPlateMethod
+//
+//  Created by Julio Enrique Sanchez Guajardo on 12/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StreakPlateMethod/StreakPlateMethod/StreakPlateMethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakPlateMethodApp.swift
+//  StreakPlateMethod
+//
+//  Created by Julio Enrique Sanchez Guajardo on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct StreakPlateMethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
