2c9d5e48131e96ef6f07106d8eca6e464dd1e867
Julio Sanchez G <je02sg@gmail.com>
2025-05-12 19:46:24 +0200

    ResultView Finish the experiment alert

--- StreakPlateMethod/StreakPlateMethod/Views/ResultView.swift
+++ StreakPlateMethod/StreakPlateMethod/Views/ResultView.swift
@@ -16,28 +16,55 @@ struct ResultView: View {
     @State var canvasView: PKCanvasView
     @State private var backgroundColor: UIColor = .white
     @State private var showingBackAlert = false
+    @State private var showingFinishAlert = false
+    @State private var cultureName: String = ""
+    @State private var viewHeight: CGFloat = 0
+    
     
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
+                
+                TextField("", text: $cultureName)
+                    .font(.title2.bold())
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .frame(width: viewHeight * 0.5)
+                    .padding(.vertical, 6)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .shadow(radius: 2)
+                    )
+                
+                
                 HStack {
                     Spacer()
                     CanvasViewResult(canvasView: $canvasView, drawing: $drawing, backgroundColor: $backgroundColor)
-                        .frame(width: geometry.size.height * 0.6, height: geometry.size.height * 0.6)
+                        .frame(width: viewHeight * 0.6, height: viewHeight * 0.6)
                         .clipShape(Circle())
                         .shadow(radius: 5)
                         .padding()
                     Spacer()
                 }
+                Button(action: {
+                    showingFinishAlert = true
+                }) {
+                    Text("Finish")
+                        .styledTextButton()
+                }
                 Spacer()
             }
-        }
-        .onAppear {
-            canvasView.tool = PKInkingTool(.pen, color: .customGray, width: 5)
-            backgroundColor = cultureMedia.color
-            //microorganismName = microorganism.name
+            .onAppear {
+                canvasView.tool = PKInkingTool(.pen, color: .customGray, width: 5)
+                backgroundColor = cultureMedia.color
+                cultureName = microorganism.name
+                //microorganismName = microorganism.name
+                viewHeight = geometry.size.height
+            }
+            .ignoresSafeArea(.keyboard)
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
@@ -53,15 +80,23 @@ struct ResultView: View {
             }
         }
         .navigationBarBackButtonHidden(true)
-
+        
         .alert("Exit the Experiment?", isPresented: $showingBackAlert) {
-            Button("Cancel", role: .cancel) {}
             Button("Exit", role: .destructive) {
                 path.removeLast(2)
                 drawing.strokes.removeAll()
             }
         } message: {
             Text("If you go back now, all your progress will be lost.")
+        }
+        .alert("Finish the Experiment?", isPresented: $showingFinishAlert) {
+            Button("Cancel", role: .destructive) {}
+            Button("Finish", role: .cancel) {
+                path.removeAll()
+                drawing.strokes.removeAll()
+            }
+        } message: {
+            Text("If you go finish now, you can't go back.")
         }
     }
 }
